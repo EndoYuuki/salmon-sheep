1f706f608f50e7b562ffa4b0ad37a5414ef834fb
EndoYuuki <shuckle.rbtech@gmail.com>
2023-08-08 13:16:16 +0900

    add tex plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown: {
+        remarkPlugins: [
+            'remark-math',
+        ],
+        rehypePlugins: [
+            ['rehype-katex', {
+            // Katex plugin options
+            }]
+        ]
+    }
+});
